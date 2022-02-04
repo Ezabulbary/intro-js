22ab1ccc90e2e11a521a769e0030b9aa3fc56092
Ezabulbary <ezabul.bary@gmail.com>
2022-02-04 23:43:52 +0600

    types of number

---
+++ conversion.js
@@ -0,0 +1,39 @@
+var firstName = "Ezabul";
+var lastName = "Bari"
+
+var fullName = firstName + " " + lastName;
+
+console.log(fullName)
+
+
+var first = "Aminul"
+var last = "Kobir"
+
+var name = first + " " + last;
+
+// console.log(name);
+
+var egg = "30";
+var milk = "50";
+
+var total = egg + milk;
+
+// console.log(total);
+
+// integer 
+
+var sunglass = 2;
+
+// float 
+
+var price = 99.99;
+
+
+var egg = "30.32";
+var milk = "50.44";
+
+var eggPrice = parseFloat(egg);
+ver milkPrice = parseInt(milk);
+
+// console.log(eggPrice);
+console.log(milkPrice)
