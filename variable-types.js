ecb26ea4effd6be1436768206afeaf08b8f4c43b
Ezabulbary <ezabul.bary@gmail.com>
2022-02-05 00:16:04 +0600

    variable types

---
+++ variable-types.js
@@ -0,0 +1,8 @@
+var tShirtPrice = 200;
+console.log(typeof tShirtPrice);
+var tShirtPrice = "200";
+console.log(typeof tShirtPrice);
+var isRaining = true;
+console.log(typeof isRaining);
+var isRommantic;
+console.log(typeof isRommantic);
